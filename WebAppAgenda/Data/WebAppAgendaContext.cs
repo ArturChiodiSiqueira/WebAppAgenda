f3eb86bc6833e528534f1c410a39338cef4f597f
artur <artur@DESKTOP-V0JU5PP>
2022-10-17 13:45:30 -0300

    Add project files.

---
+++ WebAppAgenda/Data/WebAppAgendaContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WebAppAgenda.Models;
+
+namespace WebAppAgenda.Data
+{
+    public class WebAppAgendaContext : DbContext
+    {
+        public WebAppAgendaContext (DbContextOptions<WebAppAgendaContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<WebAppAgenda.Models.Person> Person { get; set; }
+    }
+}
---
+++ WebAppAgenda/Models/Person.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace WebAppAgenda.Models
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime Birthday { get; set; }
+    }
+}
